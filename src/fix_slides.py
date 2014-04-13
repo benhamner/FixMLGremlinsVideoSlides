3b7153222513e4b83c305f62f95330c77fa449f2
Ben Hamner <ben@benhamner.com>
2014-04-13 02:53:12 -0700

    functional version

--- src/fix_slides.py
+++ src/fix_slides.py
@@ -1,15 +1,16 @@
 import cv2
+import cv2.cv as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
 video_path =  "../data/MachineLearningGremlins.mp4"
-output_path = "../data/out.mp4"
+output_path = "../data/out.avi"
 slide_paths = ["../data/Slides/Slide%d.PNG" % i for i in range(1,148)]
 
-video_slide_left   = 244
-video_slide_lower  = 67
-video_slide_upper  = 653
-video_slide_right  = 1278
+video_slide_left   = 243
+video_slide_lower  = 65
+video_slide_upper  = 655
+video_slide_right  = 1279
 video_slide_width  = video_slide_right - video_slide_left  + 1
 video_slide_height = video_slide_upper - video_slide_lower + 1
 video_slide_size   = (video_slide_width, video_slide_height)
@@ -19,30 +20,49 @@ video_slide_length = video_slide_height * video_slide_width
 slides_original = [cv2.imread(path) for path in slide_paths]
 slides_resized  = [cv2.resize(slide, video_slide_size) for slide in slides_original]
 slides_gray     = [np.reshape(cv2.cvtColor(slide, cv2.COLOR_BGR2GRAY), video_slide_length) for slide in slides_resized]
-print(np.shape(slides_gray[0]))
 
 cap    = cv2.VideoCapture(video_path)
-writer = cv2.VideoWriter(output_path, ['M','P','4','V'], 29, (1280,720), True)
+frame_rate = cap.get(cv.CV_CAP_PROP_FPS)
+fourcc     = cap.get(cv.CV_CAP_PROP_FOURCC)
+print("Frame Rate: \t%f" % cap.get(cv.CV_CAP_PROP_FPS))
+print("Frame Count:\t%d" % cap.get(cv.CV_CAP_PROP_FRAME_COUNT))
+print("FOURCC:     \t%d" % cap.get(cv.CV_CAP_PROP_FOURCC))
+xvid = cv.CV_FOURCC('X','V','I','D')
+
+writer = cv2.VideoWriter(output_path, xvid, frame_rate, (1280,720), True)
 last_cropped_gray = None
 
-for i in range(2000):
+i = 0
+ret = True
+transition_corr      = 0.0
+last_transition_corr = 0.0
+max_corr = 0.0
+min_slide = 0
+slide_width = 10
+frame = 1
+
+while frame is not None:
+    i += 1
     ret,frame = cap.read()
     cropped = frame[video_slide_lower:video_slide_upper+1, video_slide_left:video_slide_right+1, :]
     cropped_gray = np.reshape(cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY), video_slide_length)
     if last_cropped_gray is not None:
-        last_coef = np.corrcoef(last_cropped_gray, cropped_gray)[0,1]
-    else:
-        last_coef = 0.0
+        transition_corr = np.corrcoef(last_cropped_gray, cropped_gray)[0,1]
     last_cropped_gray = cropped_gray
-    if last_coef < 0.99:
-        slide_correlations = np.array([np.corrcoef(cropped_gray, slide)[0,1] for slide in slides_gray])
-        print(i, slide_correlations.max(), slide_correlations.argmax())
-        slide = slide_correlations.argmax()
+    if (transition_corr < 0.99) != (last_transition_corr < 0.99): # only change slide on transitions from/to stable state
+        slide_correlations = np.array([np.corrcoef(cropped_gray, slide)[0,1] for slide in slides_gray[min_slide:min_slide+slide_width]])
+        slide = slide_correlations.argmax() + min_slide
         max_corr = slide_correlations.max()
-        plt.subplot(121),plt.imshow(cropped)
-        plt.subplot(122),plt.imshow(slides_resized[slide_correlations.argmax()])
-        plt.show(block=False)
-        cv2.waitKey(0)
+        if max_corr>0.25:
+            cv2.imwrite("../data/Frames/%d-slide-%f.png" % (i, max_corr), slides_resized[slide])
+            cv2.imwrite("../data/Frames/%d-cropped.png" % i, cropped)
+            print(i, max_corr, slide, transition_corr, last_transition_corr)
     if max_corr > 0.5:
+        min_slide = max(slide - int(slide_width/2), 0)
         frame[video_slide_lower:video_slide_upper+1, video_slide_left:video_slide_right+1, :] = slides_resized[slide]
+    last_transition_corr = transition_corr
     writer.write(frame)
+writer.release()
+
+# .\ffmpeg.exe -i MachineLearningGremlins.mp4 -acodec copy -vn audio.mp4
+# .\ffmpeg.exe -i out.avi -i audio.mp4 -vcodec copy -acodec copy -map 0:0 -map 1:0 final.mp4
